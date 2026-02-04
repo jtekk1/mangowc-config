dc56676c9a6c1cc518c856b7b38c4bba3631949c
joaquin <joaquin@jtekk.dev>
2026-02-03 22:07:25 -0700

    .

--- autostart.sh
+++ autostart.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 set +e
 
-if [ -z "$DBUS_SESSION_BUS_ADDRESS" ]; then
-	export DBUS_SESSION_BUS_ADDRESS="unix:path=$XDG_RUNTIME_DIR/bus"
-fi
+# if [ -z "$DBUS_SESSION_BUS_ADDRESS" ]; then
+# 	export DBUS_SESSION_BUS_ADDRESS="unix:path=$XDG_RUNTIME_DIR/bus"
+# fi
 
 pkill -f xdg-desktop-portal-wlr
 pkill -f xdg-desktop-portal-gtk
@@ -16,11 +16,6 @@ export WAYLAND_DISPLAY="${WAYLAND_DISPLAY:-wayland-0}"
 
 # Update dbus environment for portals and other services
 dbus-update-activation-environment DISPLAY WAYLAND_DISPLAY XDG_CURRENT_DESKTOP XDG_SESSION_TYPE XDG_RUNTIME_DIR
-
-# dinitctl --user setenv WAYLAND_DISPLAY="$WAYLAND_DISPLAY"
-# dinitctl --user setenv DISPLAY="$DISPLAY"
-# dinitctl --user setenv XDG_RUNTIME_DIR="$XDG_RUNTIME_DIR"
-# dinitctl --user setenv XDG_CURRENT_DESKTOP="$XDG_CURRENT_DESKTOP"
 
 /usr/libexec/xdg-desktop-portal-wlr &
 sleep 1
