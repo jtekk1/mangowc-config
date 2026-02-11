7befcc34f8971932fc4c58e252168f37bb4a15c5
joaquin <joaquin@jtekk.dev>
2026-02-10 22:04:30 -0700

    .

--- autostart.sh
+++ autostart.sh
@@ -4,6 +4,8 @@ set +e
 # if [ -z "$DBUS_SESSION_BUS_ADDRESS" ]; then
 # 	export DBUS_SESSION_BUS_ADDRESS="unix:path=$XDG_RUNTIME_DIR/bus"
 # fi
+dinitctl start wireplumber
+dinitctl start pipewire-pulse
 
 pkill -f xdg-desktop-portal-wlr
 pkill -f xdg-desktop-portal-gtk
@@ -18,9 +20,10 @@ export WAYLAND_DISPLAY="${WAYLAND_DISPLAY:-wayland-0}"
 dbus-update-activation-environment DISPLAY WAYLAND_DISPLAY XDG_CURRENT_DESKTOP XDG_SESSION_TYPE XDG_RUNTIME_DIR
 
 /usr/libexec/xdg-desktop-portal-wlr &
-sleep 1
+sleep 0.2
 
 /usr/libexec/xdg-desktop-portal-gtk &
-sleep 1
+sleep 0.2
 
 /usr/libexec/xdg-desktop-portal &
+sleep
