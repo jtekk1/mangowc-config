ec5f58b18d4f17768f5e30c189c95abab7953886
joaquin <joaquin@jtekk.dev>
2026-02-17 00:03:38 -0700

    adding dinit user services

--- autostart.sh
+++ autostart.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-pkill -f xdg-desktop-portal-wlr || true
-pkill -f xdg-desktop-portal-gtk || true
-pkill -f xdg-desktop-portal || true
-
 # Set desktop environment for other apps
 export XDG_CURRENT_DESKTOP=wlroots
 export XDG_SESSION_TYPE=wayland
@@ -13,10 +9,10 @@ export WAYLAND_DISPLAY="${WAYLAND_DISPLAY:-wayland-0}"
 # Update dbus environment for portals and other services
 dbus-update-activation-environment DISPLAY WAYLAND_DISPLAY XDG_CURRENT_DESKTOP XDG_SESSION_TYPE XDG_RUNTIME_DIR
 
-$(command -v xdg-desktop-portal-wlr) &
-sleep 0.2
-
-$(command -v xdg-desktop-portal-gtk) &
-sleep 0.2
-
-$(command -v xdg-desktop-portal) &
+# $(command -v xdg-desktop-portal-wlr) &
+# sleep 0.2
+#
+# $(command -v xdg-desktop-portal-gtk) &
+# sleep 0.2
+#
+# $(command -v xdg-desktop-portal) &
