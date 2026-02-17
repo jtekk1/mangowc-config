cf594d29a30e7ec42babf803de016a53799b5f63
joaquin <joaquin@jtekk.dev>
2026-02-16 23:05:45 -0700

    .

--- autostart.sh
+++ autostart.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/bash
+#!/bin/bash
 set -e
 
-pkill -f xdg-desktop-portal-wlr
-pkill -f xdg-desktop-portal-gtk
-pkill -f xdg-desktop-portal
+pkill -f xdg-desktop-portal-wlr || true
+pkill -f xdg-desktop-portal-gtk || true
+pkill -f xdg-desktop-portal || true
 
 # Set desktop environment for other apps
 export XDG_CURRENT_DESKTOP=wlroots
@@ -13,15 +13,10 @@ export WAYLAND_DISPLAY="${WAYLAND_DISPLAY:-wayland-0}"
 # Update dbus environment for portals and other services
 dbus-update-activation-environment DISPLAY WAYLAND_DISPLAY XDG_CURRENT_DESKTOP XDG_SESSION_TYPE XDG_RUNTIME_DIR
 
-dinitctl --user setenv WAYLAND_DISPLAY="$WAYLAND_DISPLAY"
-dinitctl --user setenv DISPLAY="$DISPLAY"
-dinitctl --user setenv XDG_RUNTIME_DIR="$XDG_RUNTIME_DIR"
-dinitctl --user setenv XDG_CURRENT_DESKTOP="$XDG_CURRENT_DESKTOP"
+$(command -v xdg-desktop-portal-wlr) &
+sleep 0.2
 
-/usr/libexec/xdg-desktop-portal-wlr &
-sleep 1
+$(command -v xdg-desktop-portal-gtk) &
+sleep 0.2
 
-/usr/libexec/xdg-desktop-portal-gtk &
-sleep 1
-
-/usr/libexec/xdg-desktop-portal &
+$(command -v xdg-desktop-portal) &
